cb35c3a4641cbb09a0fb12de7f95b900f34b4fa0
jionocon <jionocon@macbook-pro.hsd1.ma.comcast.net>
2020-06-23 12:12:20 -0400

    Changed IC mechs

--- mechs/genICinput.m
+++ mechs/genICinput.m
@@ -1,9 +1,7 @@
-function sigIn = genICinput(trial, tauR, tauD, dt)
+function sigIn = genICinput(trial,tauR,tauD,dt)
 % input:
-%   tauR, tauD = rise and fall times of the EPSP waveform
-%   dt = sampling frequency of IC data
 % output:
-%   sigIn = a matrix of EPSP waveforms, with dimensions time x (nfreqs x nlocs)
+%   sigIn = a vector with 1s at spike times waveforms, with dimensions time x (nfreqs x nlocs)
 %
 % @ Erik Roberts, Kenny Chou
 % Boston Univeristy, 2019
@@ -23,7 +21,6 @@ spk_IC = fileData.spks;
 spk_IC = permute(spk_IC,[3,2,1]);
 sigIn = squeeze(spk_IC(:,:,trial)); % time x location x cells
 
-
 % ========================= create EPSP waveform =========================
 % ============= convolve each time series with epsc waveform ==============
 t_a = max(tauR,tauD)*7; % Max duration of syn conductance
@@ -34,5 +31,10 @@ tau_rise = tau1*tau2/(tau1-tau2);
 b = ((tau2/tau1)^(tau_rise/tau1) - (tau2/tau1)^(tau_rise/tau2)^-1); % /tau2?
 epsc =  - b * ( exp(-t_vec/tau1) - exp(-t_vec/tau2) ); % - to make positive
 sigIn = conv2(sigIn,epsc','same');
+% 
+% t_a = dur; % Max duration of syn conductance
+% t_vec = 0:dt:t_a;
+% current =  ones(1,length(t_vec));
+% sigIn = conv2(sigIn,current','same');
 
 end
