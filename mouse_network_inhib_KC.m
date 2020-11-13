539b7e7db107598a5cab7c1a43f80cc598d8025b
kfchou <kfchou@bu.edu>
2020-11-13 00:33:37 -0500

    Add TD neurons
    Add TD neurons, and connection from TD -> X.
    TD neurons are defaulted to be off ( Itonic = 0 ). So in this state, this addition should have no affect on your simulation outcomes.

---
+++ mouse_network_inhib_KC.m
@@ -0,0 +1,128 @@
+function simdata = mouse_network_inhib(study_dir,time_end,varies,xrNetcon,restricts)
+% [perf, fr, annotstr, distMat, VR] = mouse_network(study_dir,time_end,...
+%    varies,plot_rasters,plot_distances,data_spks,data_tau,restrict_vary_flag)
+
+% [performance, tauMax] = mouse_network(study_dir,time_end,varies,plot_rasters,data_spks)
+% study_dir: location of IC spike files + directory for log and data files
+% time_end: length of simulation in ms
+% varies: vary parameter, a structure. e.g.
+%   varies(1).conxn = '(IC->IC)';
+%   varies(1).param = 'trial';
+%   varies(1).range = 1:20;
+%   *first set of parameters should always be "trial" for IC->IC cxn
+% plot_rasters: 1 or 0
+% data_spks: spikes from data we want to match 
+%
+% @Kenny F Chou, Boston Univ. 2019-06-20
+% 2019-08-04 - added sharpening neurons
+% 2019-08-14 - plotting now handles multiple varied parameters
+% 2019-09-11 - removed redundant code. Return R performance in addition to
+%              C performance
+% 2020-02-05 - added netCons as parameter
+% to do - fix plot_rasters option
+
+% @Jio Nocon, Boston Univ., 2020-6-18
+% 2020-6-18 - added subfunction to plot model rasters vs. data rasters and
+%             calculate VR distance between the two
+
+%% Input check
+if ~strcmp(varies(1).param,'trial')
+    error('first set of varied params should be ''trial''')
+end
+
+%% solver params
+solverType = 'euler';
+dt = 1; %ms % the IC input is currently dt=1
+
+%% neuron populations
+% tonic = bias = cells spontaneous firing
+
+nCells = 4;
+s = struct();
+
+s.populations(1).name = 'IC';
+s.populations(1).equations = 'chouLIF';
+s.populations(1).size = nCells;
+
+s.populations(end+1).name = 'X';
+s.populations(end).equations = 'chouLIF';
+s.populations(end).size = nCells;
+
+s.populations(end+1).name='R';
+s.populations(end).equations = 'chouLIF';
+s.populations(end).size = nCells;
+
+s.populations(end+1).name='S';
+s.populations(end).equations = 'chouLIF';
+s.populations(end).size = nCells;
+s.populations(end).parameters = {'V_reset',-60};
+
+s.populations(end+1).name='C';
+s.populations(end).equations = 'chouLIF_adapt';
+s.populations(end).size = 1;
+s.populations(end).parameters = {'tau_ad',60};
+
+% Imask vector specifies the channels that receive Iapp
+Imask = zeros(1,nCells);
+s.populations(end+1).name='TD';
+s.populations(end).equations = 'LIF_Iapp';
+s.populations(end).size = 1;
+s.populations(end).parameters = {'Itonic',0,'noise',0,'Imask',Imask'};
+
+
+%% connections
+
+s.connections(1).direction='IC->IC';
+s.connections(1).mechanism_list={'IC_V2'};
+s.connections(1).parameters={'g_postIC',0.25,'trial',1}; % 100 hz spiking
+
+s.connections(end+1).direction='IC->X';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.18, 'tauR',0.3, 'tauD',1.5, 'netcon', eye(nCells)}; 
+
+s.connections(end+1).direction='IC->S';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.08, 'tauR',0.3, 'tauD',1.5, 'netcon', eye(nCells)}; 
+
+s.connections(end+1).direction='IC->R';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.18, 'tauR',0.3, 'tauD',1.5, 'netcon', eye(nCells)}; 
+
+s.connections(end+1).direction='X->R';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.12, 'tauR',2, 'tauD',10, 'netcon',xrNetcon, 'ESYN',-80}; 
+
+s.connections(end+1).direction='S->R';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.18, 'tauR',1.5, 'tauD',5, 'netcon', diag([1 0 0 1]),'ESYN',-80,'delay',3}; 
+
+s.connections(end+1).direction='R->C';
+s.connections(end).mechanism_list={'synDoubleExp_V2'};
+
+s.connections(end+1).direction = 'TD->X';
+s.connections(end).mechanism_list={'synDoubleExp'};
+s.connections(end).parameters={'gSYN',0.12, 'tauR',2, 'tauD',10, 'netcon',xrNetcon, 'ESYN',-80}; 
+
+%% vary params
+vary = cell(length(varies),3);
+for i = 1:length(varies)
+    vary{i,1} = varies(i).conxn;
+    vary{i,2} = varies(i).param;
+    vary{i,3} = varies(i).range;
+end
+
+%% simulate
+tic;
+
+simdata = dsSimulate(s,'tspan',[dt time_end], 'solver',solverType, 'dt',dt,...
+  'downsample_factor',1, 'save_data_flag',0, 'save_results_flag',1,...
+  'study_dir',study_dir, 'vary',vary, 'debug_flag', 0, 'verbose_flag',0,...
+  'restricts',restricts);
+
+simdata = rmfield(simdata,{'IC_V','R_V','labels','simulator_options','model'});
+
+% save(fullfile(study_dir,'simulation_results.mat'),'simdata','-v7.3');
+
+toc;
+
+end
