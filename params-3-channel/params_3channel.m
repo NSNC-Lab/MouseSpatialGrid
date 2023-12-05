c60bb06a4b0aa0cdc00d83814e2fa6f1657c0460
noconjio <noconjio@bu.edu>
2023-12-05 10:57:37 -0500

    Re-arranging things + adding cross-channel inhibitory strength

--- params-3-channel/params_3channel.m
+++ params-3-channel/params_3channel.m
@@ -4,8 +4,6 @@ options = struct;
 options.nCells = 3;
 options.opto = 0;
 
-if options.opto, nSims = 5; else, nSims = 1; end
-
 options.mex_flag = 0;
 options.parfor_flag = 0;
 options.plotRasters = 0;
@@ -14,8 +12,17 @@ options.plotRasters = 0;
 options.locNum = 15;
 options.SpatialAttention = 0;
 
+% use a separate struct for connectivity matrices (netcons) between populations
+netcons = struct; % row = source, column = target
+netcons.XRnetcon = zeros(options.nCells,options.nCells);
+netcons.XRnetcon([2 2],[1 3]) = 1;
+
+netcons.RCnetcon = ones(options.nCells,1);
+
 %% define network parameters
 clear varies
+
+if options.opto, nSims = 5; else, nSims = 1; end
 
 trialInds = repmat(1:20,nSims,1);
 
@@ -33,43 +40,25 @@ varies(end).range = 0.02;
 % R2On->C connections
 varies(end+1).conxn = 'R2On->C';
 varies(end).param = 'gSYN';
-varies(end).range = 0.018;
+varies(end).range = 0.015;
 
-% onset pvs
+% onset pv inhibition
 varies(end+1).conxn = '(S1On->R1On,S1On->R1Off,S2On->R2On,S2On->R2Off)';
 varies(end).param = 'gSYN';
 varies(end).range = 0.025;
 
-% offset pvs
+% offset pvs inhibition
 varies(end+1).conxn = '(S1Off->R1On,S1Off->R1Off,S2Off->R2On,S2Off->R2Off)';
 varies(end).param = 'gSYN';
 varies(end).range = 0.01;
 
-% control and opto conditions 
-varies(end+1).conxn = '(S1On,S1Off,S2On,S2Off)';
-varies(end).param = 'Itonic';
-varies(end).range = 0; 
+% cross-channel inhibition
+varies(end+1).conxn = '(X1->R1On,X2->R2On)';
+varies(end).param = 'gSYN';
+varies(end).range = 0.01;
 
-varies(end+1).conxn = '(R2On->R2On,S2On->S2On,S2Off->S2Off)';
+% noise at intermediate neurons
+varies(end+1).conxn = 'R2On->R2On';
 varies(end).param = 'FR';
 varies(end).range = 3;
 
-% find varied parameter, excluding trials
-varied_param = find( (cellfun(@length,{varies.range}) > 1 & ~cellfun(@iscolumn,{varies.range})));
-if numel(varies(1).range) == 20, varied_param(1) = []; % delete trial varies
-    end
-
-if isempty(varied_param) % if no varied params, settle on 2nd entry in varies
-    varied_param = 2;
-end
-
-% netcons can't be put in the varies struct (Dynasim doesn't recognize it?);
-% it needs to be put in a different struct
-
-netcons = struct; % row = source, column = target
-netcons.XRnetcon = zeros(options.nCells,options.nCells);
-netcons.RCnetcon = ones(options.nCells,1);
-
-% for simplification: use 1st varied param for 2d searches
-expVar = [varies(varied_param(1)).conxn '-' varies(varied_param(1)).param];
-expVar = strrep(expVar,'->','_');
--- params-3-channel/params_3channel_Full.m
+++ params-3-channel/params_3channel_Full.m
@@ -4,27 +4,25 @@ options = struct;
 options.nCells = 3;
 options.opto = 0;
 
-if options.opto, nSims = 5; else, nSims = 1; end
-
 options.mex_flag = 0;
 options.parfor_flag = 0;
 options.plotRasters = 0;
 
 % locNum should be empty for full grids
-% options.locNum = [];
 options.locNum = 5:24;
 options.SpatialAttention = 0;
 
-%% define network parameters
-clearvars varies netcons
-
-% netcons can't be put in the varies struct (Dynasim doesn't recognize it?);
-% it needs to be put in a different struct
-
-% row = source, column = target
-netcons = struct; 
+% use a separate struct for connectivity matrices (netcons) between populations
+netcons = struct; % row = source, column = target
 netcons.XRnetcon = zeros(options.nCells,options.nCells);
+netcons.XRnetcon([2 2],[1 3]) = 1;
+
 netcons.RCnetcon = ones(options.nCells,1);
+
+%% define network parameters
+clear varies
+
+if options.opto, nSims = 5; else, nSims = 1; end
 
 trialInds = repmat(1:20,nSims,1);
 
@@ -42,36 +40,25 @@ varies(end).range = 0.02;
 % R2On->C connections
 varies(end+1).conxn = 'R2On->C';
 varies(end).param = 'gSYN';
-varies(end).range = 0.016;
+varies(end).range = 0.015;
 
-% onset pvs
+% onset pv inhibition
 varies(end+1).conxn = '(S1On->R1On,S1On->R1Off,S2On->R2On,S2On->R2Off)';
 varies(end).param = 'gSYN';
 varies(end).range = 0.025;
 
-% offset pvs
+% offset pvs inhibition
 varies(end+1).conxn = '(S1Off->R1On,S1Off->R1Off,S2Off->R2On,S2Off->R2Off)';
 varies(end).param = 'gSYN';
 varies(end).range = 0.01;
 
-% control and opto conditions 
-varies(end+1).conxn = '(S1On,S1Off,S2On,S2Off)';
-varies(end).param = 'Itonic';
-varies(end).range = 0; 
+% cross-channel inhibition
+varies(end+1).conxn = '(X1->R1On,X2->R2On)';
+varies(end).param = 'gSYN';
+varies(end).range = 0.013;
 
-varies(end+1).conxn = '(R2On->R2On,S2On->S2On,S2Off->S2Off)';
+% noise at intermediate neurons
+varies(end+1).conxn = 'R2On->R2On';
 varies(end).param = 'FR';
 varies(end).range = 3;
 
-% find varied parameter, excluding trials
-varied_param = find( (cellfun(@length,{varies.range}) > 1 & ~cellfun(@iscolumn,{varies.range})));
-if numel(varies(1).range) == 20, varied_param(1) = []; % delete trial varies
-    end
-
-if isempty(varied_param) % if no varied params, settle on 2nd entry in varies
-    varied_param = 2;
-end
-
-% for simplification: use 1st varied param for 2d searches
-expVar = [varies(varied_param(1)).conxn '-' varies(varied_param(1)).param];
-expVar = strrep(expVar,'->','_');
